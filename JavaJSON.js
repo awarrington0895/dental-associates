ca903cfe49aad0acb92740ef68df8f2cc24c779e
Alex <awarrington0895@gmail.com>
2017-12-02 15:40:59 -0600

    Removed some uneeded comments

--- JavaJSON.js
+++ JavaJSON.js
@@ -85,45 +85,7 @@
                 }
             ]
         }
-        // // EXTRACT VALUE FOR HTML HEADER. 
-        // var col = [];
-        // for (var i = 0; i < myTable.length; i++) {
-        //     for (var key in myTable[i]) {
-        //         if (col.indexOf(key) === -1) {
-        //             col.push(key);
-        //         }
-        //     }
-        // }
 
-        // // CREATE DYNAMIC TABLE.
-        // var table = document.createElement("table");
-        // table.id = "myTable";
-
-        // // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-        // var tr = table.insertRow(-1); // TABLE ROW.
-
-        // for (var i = 0; i < col.length; i++) {
-        //     var th = document.createElement("th"); // TABLE HEADER.
-        //     th.innerHTML = col[i];
-        //     tr.appendChild(th);
-        // }
-
-        // // ADD JSON DATA TO THE TABLE AS ROWS.
-        // for (var i = 0; i < myTable.length; i++) {
-
-        //     tr = table.insertRow(-1);
-
-        //     for (var j = 0; j < col.length; j++) {
-        //         var tabCell = tr.insertCell(-1);
-        //         tabCell.innerHTML = myTable[i][col[j]];
-        //     }
-        // }
-
-        // // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        // var divContainer = document.getElementById("showData");
-        // divContainer.innerHTML = "";
-        // divContainer.appendChild(table);
     }
     window.onload = CreateTableFromJSON();
 
