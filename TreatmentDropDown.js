fcbe0c7d2f9d61eaf2b15d3b02e2ec2a0695defa
Taylor Hartwell <thartwell@ku.edu>
2017-12-02 18:34:28 -0600

    Added drop down menu for update Appointment

---
+++ TreatmentDropDown.js
@@ -0,0 +1,27 @@
+var treatmentAPI = "http://dental/api/treatments";
+var treatmentDrop = document.createElement("select");
+var options = document.createElement('option');
+
+options.innerHTML = "Select what Treatment you want";
+options.setAttribute("value", '"' + '"');
+treatmentDrop.appendChild(options);
+
+
+
+document.getElementById("div1").appendChild(treatmentDrop);
+
+
+	fetch(treatmentAPI)
+				.then((response) => response.json())
+				.then(function(data) {
+					
+                for(var i = 0; i < data.length; i++)
+				{
+				var option = document.createElement('option');
+				option.innerHTML = data[i].TxName;
+				option.setAttribute("value", '"' + i + '"');
+				treatmentDrop.appendChild(option);
+				}
+
+				})			
+
