693d8af023ad7abf2dfa0f815343206fbfc446d3
Taylor Hartwell <thartwell@ku.edu>
2017-12-03 16:26:44 -0600

    Fixed select not passing data.

--- AppointmentUpdate.js
+++ AppointmentUpdate.js
@@ -1,35 +1,34 @@
-var updateAppointmentAPI = "http://dental/api/appointment/add";
+var updateAppointmentAPI = "http://dental/api/appointment/update";
 
-function check() {
-	
-	var e = document.getElementById("dropDownMenu");
-var strUser = e.options[e.selectedIndex].text;
-alert(appid.value);
-alert(appDate.value);
-alert(appTime.value);
-alert(strUser);
-alert(Ptnum.value);
-}
+var e = document.getElementById("dropDownMenu");
 
-/*
 function commitUpdate() {
-	
+		
 	var updateRow = updateAppointmentAPI + '/' + appid.value;
 		
 		
-		 let updateObject = {
-			appt_date: appDate.value 
-			appt_time: appTime.value
-  }
-				let fetchData = {
+		 // semi colons are required after let statements
+  
+  			var updateObject = { // This will go in fetchData.  Easy tiger.  Try var instead of let this go around.  I think it also may be a scope issue.
+					appt_date: appDate.value, 
+					appt_time: appTime.value,
+					tx_name: e.options[e.selectedIndex].text,
+					pt_name: Ptnum.value
+					};
+			
+		
+				var fetchData = {
+          
 					method: 'PUT',
-					headers = new Headers()
-				}
+					body: updateObject, // Here is where the updateObject variable goes.  As in it is fine to leave it in variable form and just reference it here.
+					headers: new Headers() 
+				};
+				
 				fetch(updateRow, fetchData)
 				.then((response) => response.text())
 				.then(function(data) {
 				console.log(data);
 				});
+				
 	
 }
-*/
